293a482023adbd752888dbbc1904d5615bca58c6
csm031231 <scumim@naver.com>
2024-03-18 23:55:07 +0900

    chap 2

---
+++ 교과서 실습문제/chap 2/6번.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char a[100], b[100];
+	cout << "새 암호를 입력하세요 : ";
+	cin >> a;
+	cout << "새 암호를 다시 한 번 입력하세요 : ";
+	cin >> b;
+
+	if (strcmp(a , b) == 0) {
+		cout << "같습니다.";
+	}
+	//strcmp() 함수는 두 개의 문자열을 비교하는 함수, 두 문자열이 동일한지 비교하고, 동일하면 0을 반환
+	else {
+		cout << "다릅니다.";
+	}
+
+}
