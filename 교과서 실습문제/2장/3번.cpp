caaa1f2e232170496f7f58c25a0ac92e7388b4d0
csm031231 <scumim@naver.com>
2024-03-18 23:29:45 +0900

    교과서 실습문제

---
+++ 교과서 실습문제/2장/3번.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a, b;
+
+	cout << "두 수를 입력하라 : ";
+	cin >> a >> b;
+
+	cout << "큰 수  = ";
+
+	if (a > b){
+		cout << a;
+	}
+	else {
+		cout << b << "\n";
+	}
+
+	return 0;
+}
