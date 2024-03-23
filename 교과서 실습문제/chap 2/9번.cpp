e6026cbae04ace14f72aa848a6078167283055a9
csm031231 <144320859+csm031231@users.noreply.github.com>
2024-03-23 21:07:07 +0900

    빈칸을 포함하는 문자열 읽기

---
+++ 교과서 실습문제/chap 2/9번.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	char name[100], adress[100], age[100];
+
+	cout << "이름은? ";
+	cin.getline(name, 100);
+
+	cout << "주소는? ";
+	cin.getline(adress, 100);
+
+	cout << "나이는? ";
+	cin.getline(age, 100);
+
+	cout << name << "," << adress << "," << age << "세" << endl;
+
+	
+	return 0;
+}
